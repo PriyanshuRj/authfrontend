917f6bfab1d3e948680296ef58e48a0fb40c6cd4
PriyanshuRj <priyanshurajput0071109@gmail.com>
2022-05-26 10:21:51 +0530

    Updated login form

--- src/form/login.js
+++ src/form/login.js
@@ -1,26 +1,47 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {Link} from "react-router-dom";
+
+
 import "./style.css";
 export default function Login() {
+  const [emailValidator,validate] = useState(null);
+
+  const validateEmail = (e) => {
+    var email = e.target.value;
+    const regex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    if(!email || regex.test(email) === false){
+      validate('*Enter valid Email');
+    }
+    else{
+      const len = email.length;
+      console.log(email.substring(len-3,len));
+      if(email.substring(len-3,len)==='com'){
+        console.log("yes");
+      }
+      validate(null);
+    }
+  }
+
   return (
     <div>
       <form>
   
-  <div class="segment">
+  <div className="segment">
     <h1>Log in</h1>
   </div>
   
   <label>
-    <input type="text" placeholder="Email Address"/>
+    <input type="text" placeholder="Email Address" onChange={(e) => validateEmail(e)}/>
   </label>
+  <p className = "signuperrors">{emailValidator}</p>
   <label>
     <input type="password" placeholder="Password"/>
   </label>
-  <button class="red" type="button"><i class="icon ion-md-lock"></i> Log in</button>
+  <button className="red" type="button"><i className="icon ion-md-lock"></i> Log in</button>
   
-  <div class="segment">
+  <div className="segment">
     <p>don't have account </p>
-    <button class="unit" type="button"> <Link to="/signup">SignUp</Link></button>
+    <button className="unit" type="button"> <Link to="/signup">SignUp</Link></button>
 
   </div>
 </form>
--- src/form/signup.js
+++ src/form/signup.js
@@ -1,28 +1,74 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {Link} from "react-router-dom";
+import validator from 'validator';
 import "./style.css";
 export default function Signup() {
+  const [emailValidator,validate] = useState(null);
+  const [nameValidator,validatename] = useState(null);
+  const [passwordValidator,validatepassword] = useState(null);
+  const validateEmail = (e) => {
+    var email = e.target.value
+  
+    if (validator.isEmail(email)) {
+      validate(null);
+    } else {
+      validate('*Enter valid Email')
+    }
+  }
+  const validateName = (e) =>{
+    var name = e.target.value;
+    let state = 0;
+    for(let i = 0;i<name.length;i++){
+      if(name.charCodeAt(i)>=65 && name.charCodeAt(i)<=90){
+        continue;
+      }
+      if(name.charCodeAt(i)>=97 && name.charCodeAt(i)<=122){
+        continue;
+      }
+      else{
+        state = 1;
+        validatename("*The name can only contain letters");
+      }
+
+    }
+    if(state===0){
+      validatename(" ");
+    }
+  }
+  const validatePassword = (e) =>{
+    var password = e.target.value;
+    if(password.length>=8){
+      validatepassword("");
+    }
+    else{
+      validatepassword("* Password must be of atlest length 8");
+
+    }
+  }
   return (
     <div>
       <form>
   
-  <div class="segment">
+  <div className="segment">
     <h1>Sign up</h1>
   </div>
   <label>
-    <input type="text" placeholder="Full Name"/>
+    <input type="text" placeholder="Full Name" onChange={(e) => validateName(e)}/>
   </label>
+  <p className = "signuperrors">{nameValidator}</p>
   <label>
-    <input type="text" placeholder="Email Address"/>
+    <input type="text" placeholder="Email Address" onChange={(e) => validateEmail(e)}/>
   </label>
+  <p className = "signuperrors">{emailValidator}</p>
   <label>
-    <input type="password" placeholder="Password"/>
+    <input type="password" placeholder="Password" onChange={(e) => validatePassword(e)}/>
   </label>
-  <button class="red" type="button"><i class="icon ion-md-lock"></i> Sign Up</button>
+  <p className = "signuperrors">{passwordValidator}</p>
+  <button className="red" type="button"><i className="icon ion-md-lock"></i> Sign Up</button>
   
-  <div class="segment">
+  <div className="segment">
     <p>Already have account </p>
-    <button class="unit" type="button"><Link to="/Login">Login</Link></button>
+    <button className="unit" type="button"><Link to="/Login">Login</Link></button>
 
   </div>
 </form>
